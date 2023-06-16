fe7c0672586a5cc7b14cad8e70ca292537e0323e
sizzle01 <talktoaliyu@gmail.com>
2023-06-16 15:45:57 +0100

    installed tailwind css and set up router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-/**root routes */
+import Username from './components/Username';
+import PageNotFount from './components/PageNotFount';
+import Password from './components/Password'; 
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
 
+
+
+/**root routes */
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>Root Router</div>
+        element: <Username></Username>
     },
     {
         path: '/register',
-        element: <div>Register</div>
+        element: <Register></Register>
     },
+    {
+      path: '/password',
+      element: <Password></Password>
+  },
+  {
+    path: '/profile',
+    element: <Profile></Profile>
+},
+{
+  path: '/recovery',
+  element: <Recovery></Recovery>
+},
+{
+  path: '/reset',
+  element: <Reset></Reset>
+},
+{
+  path: '*',
+  element: <PageNotFount></PageNotFount>
+},
 
 ])
 export default function App() {
